b167e84bfe1d8d7cb6840e46a2f3620dfae20f85
mitjakrebs <meetya@freenet.de>
2023-01-27 16:52:17 +0100

    Edit

--- nano-theme-dark.el
+++ nano-theme-dark.el
@@ -21,15 +21,15 @@
   "Apply dark Nano theme base."
   ;; Colors from Nord theme at https://www.nordtheme.com
   (setq frame-background-mode     'dark)
-  (setq nano-color-foreground "#ECEFF4") ;; Snow Storm 3  / nord  6
-  (setq nano-color-background "#2E3440") ;; Polar Night 0 / nord  0
+  (setq nano-color-foreground "#DCDCCC") ;; Snow Storm 3  / nord  6
+  (setq nano-color-background "#3F3F3F") ;; Polar Night 0 / nord  0
   (setq nano-color-highlight  "#3B4252") ;; Polar Night 1 / nord  1
   (setq nano-color-critical   "#EBCB8B") ;; Aurora        / nord 11
-  (setq nano-color-salient    "#81A1C1") ;; Frost         / nord  9
-  (setq nano-color-strong     "#ECEFF4") ;; Snow Storm 3  / nord  6
-  (setq nano-color-popout     "#D08770") ;; Aurora        / nord 12
+  (setq nano-color-salient    "#D0BF8F") ;; Frost         / nord  9
+  (setq nano-color-strong     "#7F9F7F") ;; Snow Storm 3  / nord  6
+  (setq nano-color-popout     "#CC9393") ;; Aurora        / nord 12
   (setq nano-color-subtle     "#434C5E") ;; Polar Night 2 / nord  2
-  (setq nano-color-faded      "#677691") ;;
+  (setq nano-color-faded      "#494949") ;;
   ;; to allow for toggling of the themes.
   (setq nano-theme-var "dark")
   )
